3997e9a2282cc48d03c92a77676b0a54f03d49e2
Kerry Buckley <kerryjbuckley@gmail.com>
2024-01-16 14:56:08 +0000

    Add a case statement

--- elixir/lib/supermarket/model/shopping_cart.ex
+++ elixir/lib/supermarket/model/shopping_cart.ex
@@ -48,60 +48,64 @@ defmodule Supermarket.Model.ShoppingCart do
   defp calculate_discount(catalog, offer, product, quantity) do
     unit_price = SupermarketCatalog.get_unit_price(catalog, product)
     quantity_as_int = trunc(quantity)
-    discount = nil
-    qualifying_quantity = 1
 
-    {discount, qualifying_quantity} =
-      cond do
-        offer.offer_type == :three_for_two ->
-          {discount, 3}
+    case offer.offer_type do
+      _ ->
+        discount = nil
+        qualifying_quantity = 1
 
-        offer.offer_type == :two_for_amount ->
-          if quantity_as_int >= 2 do
-            qualifying_quantity = 2
-            int_division = div(quantity_as_int, qualifying_quantity)
-            price_per_unit = offer.argument * int_division
-            the_total = Integer.mod(quantity_as_int, 2) * unit_price
-            total = price_per_unit + the_total
-            discount_n = unit_price * quantity - total
-            {Discount.new(product, "2 for #{offer.argument}", -discount_n), 2}
-          else
-            {discount, qualifying_quantity}
+        {discount, qualifying_quantity} =
+          cond do
+            offer.offer_type == :three_for_two ->
+              {discount, 3}
+
+            offer.offer_type == :two_for_amount ->
+              if quantity_as_int >= 2 do
+                qualifying_quantity = 2
+                int_division = div(quantity_as_int, qualifying_quantity)
+                price_per_unit = offer.argument * int_division
+                the_total = Integer.mod(quantity_as_int, 2) * unit_price
+                total = price_per_unit + the_total
+                discount_n = unit_price * quantity - total
+                {Discount.new(product, "2 for #{offer.argument}", -discount_n), 2}
+              else
+                {discount, qualifying_quantity}
+              end
+
+            true ->
+              {discount, 2}
           end
 
-        true ->
-          {discount, 2}
-      end
+        qualifying_quantity =
+          if offer.offer_type == :five_for_amount, do: 5, else: qualifying_quantity
 
-    qualifying_quantity =
-      if offer.offer_type == :five_for_amount, do: 5, else: qualifying_quantity
+        discount_count = div(quantity_as_int, qualifying_quantity)
 
-    discount_count = div(quantity_as_int, qualifying_quantity)
+        cond do
+          offer.offer_type == :three_for_two and quantity_as_int > 2 ->
+            discount_amount =
+              quantity * unit_price -
+                (discount_count * 2 * unit_price + Integer.mod(quantity_as_int, 3) * unit_price)
 
-    cond do
-      offer.offer_type == :three_for_two and quantity_as_int > 2 ->
-        discount_amount =
-          quantity * unit_price -
-            (discount_count * 2 * unit_price + Integer.mod(quantity_as_int, 3) * unit_price)
+            Discount.new(product, "3 for 2", -discount_amount)
 
-        Discount.new(product, "3 for 2", -discount_amount)
+          offer.offer_type == :ten_percent_discount ->
+            Discount.new(
+              product,
+              "#{offer.argument}% off",
+              -quantity * unit_price * offer.argument / 100.0
+            )
 
-      offer.offer_type == :ten_percent_discount ->
-        Discount.new(
-          product,
-          "#{offer.argument}% off",
-          -quantity * unit_price * offer.argument / 100.0
-        )
+          offer.offer_type == :five_for_amount and quantity_as_int >= 5 ->
+            discount_total =
+              unit_price * quantity -
+                (offer.argument * discount_count + Integer.mod(quantity_as_int, 5) * unit_price)
 
-      offer.offer_type == :five_for_amount and quantity_as_int >= 5 ->
-        discount_total =
-          unit_price * quantity -
-            (offer.argument * discount_count + Integer.mod(quantity_as_int, 5) * unit_price)
+            Discount.new(product, "#{qualifying_quantity} for #{offer.argument}", -discount_total)
 
-        Discount.new(product, "#{qualifying_quantity} for #{offer.argument}", -discount_total)
-
-      true ->
-        discount
+          true ->
+            discount
+        end
     end
   end
 end
